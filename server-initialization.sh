9e98c3434fbdf1be7104522426cb7abd8935717f
Nicholas Ferrara <nick@nicholasferrara.net>
2016-03-27 00:30:17 -0400

    Removed bugs from server initialization script

--- server-initialization.sh
+++ server-initialization.sh
@@ -3,31 +3,45 @@ if [ ! -f /home/ubuntu/automated/custom.log ]
     then
         mkdir /home/ubuntu/automated/
         cd /home/ubuntu/automated/
-        echo "Proccess began at " >> /home/ubuntu/automated/custom.log
+        echo "Proccess began: " >> /home/ubuntu/automated/custom.log
         date >> /home/ubuntu/automated/custom.log
         exec >> /home/ubuntu/automated/custom.log 2>&1
-        add-apt-repository ppa:ondrej/php5-5.6
+        add-apt-repository -y ppa:ondrej/php5-5.6
         apt-get update -y && apt-get dist-upgrade -y
         apt-get autoremove -y && apt-get autoclean -y
         apt-get install python-software-properties
-        curl https://bootstrap.pypa.io/get-pip.py -O /home/ubuntu/automated/
+        curl https://bootstrap.pypa.io/get-pip.py -o /home/ubuntu/automated/get-pip.py
         python3.4 /home/ubuntu/automated/get-pip.py
-        apt-get install ruby2.0 apache2 php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-fpm php5-gd libssh2-php mysql-server php5-mysqlnd git unzip zip postfix php5-curl mailutils php5-json phpmyadmin php-pear -y
+        apt-get install ruby2.0 apache2 php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-fpm php5-gd libssh2-php php5-mysqlnd git unzip zip php5-curl mailutils php5-json php-pear -y4
+        debconf-set-selections <<< 'mysql-server mysql-server/root_password password codebook'
+        debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password codebook'
+        debconf-set-selections <<< "postfix postfix/mailname string 'bearcatexchange.com'"
+        debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+        echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+        echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+        echo "phpmyadmin phpmyadmin/mysql/admin-user string root" | debconf-set-selections
+        echo "phpmyadmin phpmyadmin/mysql/admin-pass password codebook" | debconf-set-selections
+        echo "phpmyadmin phpmyadmin/mysql/app-pass password codebook" |debconf-set-selections
+        echo "phpmyadmin phpmyadmin/app-password-confirm password codebook" | debconf-set-selections
+        apt-get install postfix phpmyadmin mysql-server -y
         php5enmod mcrypt
         pear install mail
-        curl -sS https://getcomposer.org/installer /var/www/be/live/ | php
+        curl https://getcomposer.org/installer -o /var/www/be/live/composer.phar | php
         pip3.4 install awscli django
-        cd /var/www/
         aws s3 cp s3://aws-codedeploy-us-east-1/latest/install . --region us-east-1
         chmod +x ./install
         ./install auto
+        mv install automated/
         service codedeploy-agent start
         service codedeploy-agent status
+        cd /var/www/
         service apache2 restart
         mkdir /var/www/be/live/ -p
         chown -R $USER:$USER /var/www/be
         chmod -R 755 /var/www
         rm /var/www/html/index.html
+        apt-get autoremove -y
+        apt-get autoclean -y
 
 #        This must be done manually:
 #        nano /etc/phpmyadmin/config.inc.php
